08ce8cf21ec50f41c016867998597a3bf31ae009
wendigo <xanderkgeorge@gmail.com>
2024-09-26 19:41:12 -0400

    added the ability to pipe to rat

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use std::fs;
 use std::env;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    let mut pipe = String::new();
+    let help = "--help";
+
+    if args[1] == help {
+        println!("\nUsage: rat [FILE]\n");
+        println!("    --help    prints this help page");
+        println!("\nExample: rat example.txt");
+        println!("Example: ls | rat\n");
+    }
+
+    else    if args.len() >= 2 {
         let file = &args[1];
         let content = fs::read_to_string(file)
             .expect("Failed to read the file");
         println!("\n{}", content);
-        println!(": End of file\n")
     }
-    else {
-        println!("\nUsage: rat [FILE]");
-        println!("Example: rat example.txt\n")
+    else if let Ok(_) = io::stdin().read_to_string(&mut pipe) {
+        println!("{}",pipe)
     }
+
 }
